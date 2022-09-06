81e757de9f14b48507fc24534dac411e47738ba1
Eduarda Alves Pontel <109531518+eduardapontel@users.noreply.github.com>
2022-09-05 22:10:48 -0300

    Delete Exercicio_008.py

--- Exercicio_008.py
+++
@@ -1,3 +0,0 @@
-m = float(input('Digite o valor em metros: '))
-print('O valor em metros corresponde a: \n{}km \n{}hm \n{}dam \n{}dm \n{}cm \n{}mm'.format(m/1000, m/100, m/10, m*10, m*100, m*1000))
-
